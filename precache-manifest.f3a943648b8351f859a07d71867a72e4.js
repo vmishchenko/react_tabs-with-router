43c967b4d3b1a167e51f7fe216994c67fd592cce
vmishchenko <vladislav.a.mishchenko@gmail.com>
2019-12-27 00:24:37 +0200

    Updates

---
+++ precache-manifest.f3a943648b8351f859a07d71867a72e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e7b2f2395d3ddd8709e931a16385b9",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "cbfcc1553cedd8180c97",
+    "url": "/react_tabs-with-router/static/css/main.29d83670.chunk.css"
+  },
+  {
+    "revision": "25a075ac788fca0c6b66",
+    "url": "/react_tabs-with-router/static/js/2.5e8ea60e.chunk.js"
+  },
+  {
+    "revision": "cbfcc1553cedd8180c97",
+    "url": "/react_tabs-with-router/static/js/main.d45ebe12.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
